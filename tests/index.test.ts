c8741238d2a4735d8e28628195d121695bc71d2a
Adhiraj Kinlekar <adhirajkinlekar@outlook.com>
2024-12-14 19:25:00 +0530

    tests : add a failing test - "should return the sum of numbers separated by commas and newlines"

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,22 +1,22 @@
-// StringCalculator.test.ts
+// https://blog.incubyte.co/blog/tdd-assessment/
+
 import { StringCalculator } from '../src/index';
 
 describe('StringCalculator', () => {
+
   let calculator: StringCalculator;
 
-  beforeEach(() => {
-    calculator = new StringCalculator();
-  });
+  beforeEach(() => calculator = new StringCalculator());
 
-  it('should return 0 for an empty string', () => {
-    expect(calculator.add('')).toBe(0);
-  });
+  it('should return 0 for an empty string', 
+    () => expect(calculator.add('')).toBe(0));
 
-  it('should return the number when a single number is provided', () => {
-    expect(calculator.add('1')).toBe(1);
-  });
+  it('should return the number when a single number is provided', 
+    () => expect(calculator.add('1')).toBe(1));
 
-  it('should return the sum of numbers delimited by a comma', () => {
-    expect(calculator.add("1,2,3")).toBe(6); 
-  });
+  it('should return the sum of numbers delimited by a comma', 
+    () => expect(calculator.add("1,2,3")).toBe(6)); 
+
+  it('should return the sum of numbers separated by commas and newlines', 
+    () => expect(calculator.add("1\n2,3")).toBe(6));
 });
