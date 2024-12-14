683d2d5abaccceb32719e9da1e255e47274b9916
Adhiraj Kinlekar <adhirajkinlekar@outlook.com>
2024-12-14 21:17:24 +0530

    tests : add a failing test - "should return the sum of numbers separated by custom delimeters and new lines"

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,13 @@
 export class StringCalculator {
 
     public add(numbers: string): number {
-        
-        if (numbers === "") return 0; 
 
-        const parsedNumbers: number[] = numbers.split(/[\n,]/).map((x)=> parseInt(x));
+        if (numbers === "") return 0;
 
-        if(parsedNumbers.length == 1) return parsedNumbers[0];
+        const parsedNumbers: number[] = numbers.split(/[\n,]/).map((x) => parseInt(x));
+
+        if (parsedNumbers.length == 1) return parsedNumbers[0];
 
         return parsedNumbers.reduce((acc, num) => acc + num, 0);
     }
-  }
-  
+}
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -4,19 +4,23 @@ import { StringCalculator } from '../src/index';
 
 describe('StringCalculator', () => {
 
-  let calculator: StringCalculator;
+    let calculator: StringCalculator;
 
-  beforeEach(() => calculator = new StringCalculator());
+    beforeEach(() => calculator = new StringCalculator());
 
-  it('should return 0 for an empty string', 
-    () => expect(calculator.add('')).toBe(0));
+    it('should return 0 for an empty string',
+        () => expect(calculator.add('')).toBe(0));
 
-  it('should return the number when a single number is provided', 
-    () => expect(calculator.add('1')).toBe(1));
+    it('should return the number when a single number is provided',
+        () => expect(calculator.add('1')).toBe(1));
 
-  it('should return the sum of numbers delimited by a comma', 
-    () => expect(calculator.add("1,2,3")).toBe(6)); 
+    it('should return the sum of numbers delimited by a comma',
+        () => expect(calculator.add("1,2,3")).toBe(6));
 
-  it('should return the sum of numbers separated by commas and newlines', 
-    () => expect(calculator.add("1\n2,3")).toBe(6));
+    // This test can optionally be removed since the case is handled in the preceeding test 
+    it('should return the sum of numbers separated by commas and new lines',
+        () => expect(calculator.add("1\n2,3")).toBe(6));
+
+    it('should return the sum of numbers separated by custom delimeters and new lines',
+        () => expect(calculator.add("//:1\n2:3")).toBe(6));
 });
