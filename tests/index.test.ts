5e3de4bc787bde34801a08be7310b19144830ce3
Adhiraj Kinlekar <adhirajkinlekar@outlook.com>
2024-12-14 18:33:29 +0530

    tests : added a failing test - "should return the number when a single number is provided"

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -11,4 +11,8 @@ describe('StringCalculator', () => {
   it('should return 0 for an empty string', () => {
     expect(calculator.add('')).toBe(0);
   });
+
+  it('should return the number when a single number is provided', () => {
+    expect(calculator.add('1')).toBe(1);
+  });
 });
